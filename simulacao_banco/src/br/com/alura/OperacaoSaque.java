aa5ef91076bcfaaad3dff3946dedf3a599d49f5b
rafael-rochaalmeida <rafael.almeida@bandtec.com.br>
2025-03-27 17:01:15 -0300

    initial commit

---
+++ simulacao_banco/src/br/com/alura/OperacaoSaque.java
@@ -0,0 +1,44 @@
+package br.com.alura;
+
+import java.math.BigDecimal;
+
+public class OperacaoSaque implements Runnable{
+
+    private Conta conta;
+    private BigDecimal valor;
+
+    public OperacaoSaque(Conta conta, BigDecimal valor) {
+        this.conta = conta;
+        this.valor = valor;
+    }
+
+    public synchronized void executa() {
+
+        System.out.println("Iniciando operação de saque.");
+
+        System.out.println("**********************************************");
+
+        var saldoAtual = conta.getSaldo();
+
+        if (saldoAtual.compareTo(valor) >= 0) {
+
+            System.out.println("Debitando valor da conta");
+            System.out.println("**********************************************");
+
+            conta.debitaSaldo(valor);
+
+            System.out.println("Saldo atual: " +conta.getSaldo());
+            System.out.println("**********************************************");
+        }
+        System.out.println("Finalizando operação de saque.");
+        System.out.println("**********************************************");
+    }
+
+    @Override
+    public  void run() {
+        
+        executa();
+        System.out.println(Thread.currentThread().getName());
+
+    }
+}
